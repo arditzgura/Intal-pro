66df358191c11e14859a5c31c20ebe51918af31b
arditzgura-debug <arditzgura@gmail.com>
2026-07-28 07:37:42 +0200

    Deploy

--- electron/preload.cjs
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  saveToDesktop:   (buffer, fileName) => ipcRenderer.invoke('save-to-desktop', { buffer, fileName }),
-  shareImage:      (buffer, fileName) => ipcRenderer.invoke('share-image',     { buffer, fileName }),
-  savePdf:         (buffer, fileName) => ipcRenderer.invoke('save-pdf',        { buffer, fileName }),
-  printWithDialog: (options)          => ipcRenderer.invoke('print-with-dialog', options),
-  getPrinters:     ()                 => ipcRenderer.invoke('get-printers'),
-});
