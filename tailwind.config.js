74cc4f8169e17361a40214a3e3b66540b1af3757
arditzgura-debug <arditzgura@gmail.com>
2026-07-29 09:48:03 +0200

    Add Tailwind PostCSS build - remove CDN dependency

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './*.tsx',
+    './components/**/*.tsx',
+    './utils/**/*.ts',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
