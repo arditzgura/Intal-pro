339ba6a652ffe715aca8865e96594dcb4257b9de
arditzgura-debug <arditzgura@gmail.com>
2026-08-07 10:34:28 +0200

    revert: restore original styles - fix invisible sidebar text

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,6 @@ export default {
     'components/**/*.tsx',
     'utils/**/*.ts',
   ],
-  theme: {
-    extend: {
-      fontWeight: {
-        thin:       '600',
-        extralight: '600',
-        light:      '600',
-        normal:     '700',
-        medium:     '700',
-        semibold:   '800',
-        bold:       '800',
-        extrabold:  '900',
-        black:      '900',
-      },
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 }
